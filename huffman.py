ecfc62be39ec32840ff3597f27fd368fc82618ca
valuvilla <valuvp@hotmail.com>
2023-05-25 09:08:41 +0200

    huffman hecho

--- huffman.py
+++ huffman.py
@@ -1,85 +1,63 @@
-class NodoHuffman:
-    def __init__(self, valor, frecuencia):
-        self.valor = valor
-        self.frecuencia = frecuencia
-        self.izquierda = None
-        self.derecha = None
- 
-    def __lt__(self, otro):     
-        return self.frecuencia < otro.frecuencia
- 
-    def __eq__(self, otro):    
-        if otro == None:
-            return False
-        if not isinstance(otro, NodoHuffman):
-            return False
-        return self.frecuencia == otro.frecuencia
- 
+class Node:
+    def __init__(self, symbol=None, frequency=None):
+        self.symbol = symbol
+        self.frequency = frequency
+        self.left = None
+        self.right = None
 
-class Arbol:
-    def calcular_frecuencias(mensaje):
-        frecuencias = {}
-        for caracter in mensaje:
-            if caracter in frecuencias:
-                frecuencias[caracter] += 1
-            else:
-                frecuencias[caracter] = 1
-        return frecuencias
-    
-    def construir_arbol_huffman(frecuencias):
-        nodos = []
-        for caracter, frecuencia in frecuencias.items():
-            nodos.append(NodoHuffman(caracter, frecuencia))
-        while len(nodos) > 1:
-            nodos.sort()
-            nodo_izq = nodos.pop(0)
-            nodo_der = nodos.pop(0)
-            nodo_padre = NodoHuffman(None, nodo_izq.frecuencia + nodo_der.frecuencia)
-            nodo_padre.izquierda = nodo_izq
-            nodo_padre.derecha = nodo_der
-            nodos.append(nodo_padre)
-        return nodos[0]
-    
-    def codificar_caracter(nodo, caracter, codigo_actual):
-        if nodo == None:
-            return None
-        if nodo.valor == caracter:
-            return codigo_actual
-        codigo_izq = Arbol.codificar_caracter(nodo.izquierda, caracter, codigo_actual + "0")
-        if codigo_izq != None:
-            return codigo_izq
-        codigo_der = Arbol.codificar_caracter(nodo.derecha, caracter, codigo_actual + "1")
-        return codigo_der
-    
-    def codificar_mensaje(mensaje, arbol_huffman):
-        mensaje_codificado = ""
-        for caracter in mensaje:
-            codigo = Arbol.codificar_caracter(arbol_huffman, caracter, "")
-            mensaje_codificado += codigo
-        return mensaje_codificado
-    
-    def decodificar_mensaje(mensaje_codificado, arbol_huffman):
-        mensaje_decodificado = ""
-        nodo_actual = arbol_huffman
-        for bit in mensaje_codificado:
-            if bit == "0":
-                nodo_actual = nodo_actual.izquierda
-            elif bit == "1":
-                nodo_actual = nodo_actual.derecha
-            if nodo_actual.valor != None:
-                mensaje_decodificado += nodo_actual.valor
-                nodo_actual = arbol_huffman
-        return mensaje_decodificado
-    
+def get_nodes(frequencies):
+    return sorted([Node(symbol, frequency) for symbol, frequency in frequencies.items()], key=lambda x: x.frequency)
 
-if __name__ == "__main__":
+def huffman_tree(nodes):
+    while len(nodes) > 1:
+        nodes = sorted(nodes, key=lambda x: x.frequency)
+        left = nodes.pop(0)
+        right = nodes.pop(0)
+        inner = Node(frequency=left.frequency + right.frequency)
+        inner.left = left
+        inner.right = right
+        nodes.append(inner)
+    return nodes[0]
 
-    frecuencias = {"T":0.15, "O":0.15, "A":0.12, "E": 0.10, "H":0.09, "S": 0.07, "P": 0.07, "M": 0.07, "N": 0.06, "C": 0.06, "D":0.05, "Z":0.04, "K": 0.03, " ": 0.03}
-    arbol_huffman = Arbol.construir_arbol_huffman(frecuencias)
-    mensaje_coficado = "0011111010111001100110111010"
-    mensaje_decodificado = Arbol.decodificar_mensaje(mensaje_coficado, arbol_huffman)
-    print(mensaje_decodificado)
-    # coficar mensaje "hazte con todos los pokemon"
-    mensaje = 
-    mensaje_codificado2 = Arbol.codificar_mensaje(mensaje, arbol_huffman)
-    print(mensaje_codificado2)
+def huffman_encoding(node, prefix='', code={}):
+    if node is None:
+        return
+    if node.symbol is not None:
+        code[node.symbol] = prefix
+    huffman_encoding(node.left, prefix + '0', code)
+    huffman_encoding(node.right, prefix + '1', code)
+    return code
+
+def compress(message, code):
+    return ''.join([code[symbol] for symbol in message])
+
+def decompress(compressed, node):
+    message = ''
+    current = node
+    for bit in compressed:
+        if bit == '0':
+            current = current.left
+        else:
+            current = current.right
+        if current.symbol is not None:
+            message += current.symbol
+            current = node
+    return message
+
+# Diccionario de frecuencias
+frequencies = {"T":0.15, "O":0.15, "A":0.12, "E": 0.10, "H":0.09, "S": 0.07, "P": 0.07, "M": 0.07, "N": 0.06, "C": 0.06, "D":0.05, "Z":0.04, "K": 0.03, " ": 0.03}
+# Generación del árbol de Huffman
+nodes = get_nodes(frequencies)
+huff_tree = huffman_tree(nodes)
+huff_code = huffman_encoding(huff_tree)
+
+# Mensaje a comprimir
+message = 'HAZTE CON TODOS POKEMON'
+
+# Compresión y descompresión
+compressed = compress(message, huff_code)
+decompressed = decompress(compressed, huff_tree)
+
+print(f'Mensaje original: {message}')
+print(f'Mensaje comprimido: {compressed}')
+print(f'Mensaje descomprimido: {decompressed}')
